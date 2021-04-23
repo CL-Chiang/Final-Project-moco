80a8867164aa7009747228c79d4f3f824c20f859
unknown <igtaylor@umich.edu>
2021-04-23 18:25:47 -0400

    isabels comp

--- moco/reformat_imagenet.py
+++ moco/reformat_imagenet.py
@@ -2,10 +2,12 @@
 import unpickle as up
 from PIL import Image
 
-filename = "/home/tyler/PycharmProjects/val_imagenet_downsampled/val_data"
-dir = "/home/tyler/PycharmProjects/train/"
+filename = 'val_data'
+print('HERE')
+#attempt = 't1'
 d = up.unpickle(filename)
 data = d['data']
+print(data.shape)
 n = data.shape[0]
 img_size = 64
 num_channels = 3
@@ -13,4 +15,4 @@ for i in range(n):
     arr = data[i].reshape(img_size,img_size,3,order="F")
     im = Image.fromarray(arr)
     im = im.rotate(270)
-    im.save(dir + str(i) + ".jpg")
+    im.save(str(i) + ".jpg")
